7254b6d1ed8f473c8b85bc65c701384a8d969611
Lucasluis1407 <lucasluisouza@gmail.com>
2023-09-02 01:08:18 -0300

    add responsive & add API & Fix gitignore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    API_KEY: "01e6e3ee4b8848dab6bab439ca562ef8",
+};
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    API_KEY: "01e6e3ee4b8848dab6bab439ca562ef8"
+};
